e6d92a66ed6728032ef0cac04adeec103bc40dac
Michal Granatowski <m.granatowski@gmail.com>
2022-02-21 22:40:04 +0100

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("HelloWorld");
+        System.out.println("Michal");
+        System.out.println(25);
+        System.out.println('G');
+    }
+
+}
