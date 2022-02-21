722ccdbbb2bdcbd1a17c3b8a548fc92769f9953d
Michal Granatowski <m.granatowski@gmail.com>
2022-02-21 23:25:00 +0100

    Zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myCar = "Honda";
+        double engine = 1.8;
+        int power = 140;
+        char numberOfDoors = '5';
+        boolean isHatchback = true;
+        String engineType = "i-VTEC";
+        System.out.println(myCar);
+        System.out.println(engineType);
+        System.out.println(engine);
+        System.out.println(power);
+        System.out.println(numberOfDoors);
+        System.out.println(isHatchback);
+    }
+}
