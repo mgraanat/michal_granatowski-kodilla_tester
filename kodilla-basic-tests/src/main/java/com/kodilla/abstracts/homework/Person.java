62db1cdbb13a950e862e1edcd59faa81f9abb2fd
Michal Granatowski <m.granatowski@gmail.com>
2022-03-26 13:16:00 +0100

    Job2

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
@@ -3,20 +3,23 @@ package com.kodilla.abstracts.homework;
 public class Person {
     String firstName;
     int age;
-    String job;
 
-    public Person (String firstName, int age, String job){
+    public Person(String firstName, int age) {
         this.firstName = firstName;
         this.age = age;
-        this.job = job;
     }
 
     public static void main(String[] args) {
-        Person Michal = new Person("Michal", 25, "Mechanic");
-        Person Daniel = new Person("Daniel", 50, "Doctor");
-        Person Cristiano = new Person("Cristiano", 37, "Footballer");
+        Person Michal = new Person("Michal", 25);
+        Person Daniel = new Person("Daniel", 50);
+        Person Cristiano = new Person("Cristiano", 37);
 
-        Person[] persons = {Michal,Daniel,Cristiano};
+        Person[] persons = {Michal, Daniel, Cristiano};
+
+        Job mechanic = new Mechanic();
+
+        System.out.println(Michal.firstName);
+        System.out.println(mechanic.getResponsibilities());
 
 
     }
