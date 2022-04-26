9a35a59476f7df50e2bc8d5a3a89a8b129e6cae1
Michal Granatowski <m.granatowski@gmail.com>
2022-04-26 18:28:40 +0200

    Order - exception

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -0,0 +1,30 @@
+package com.kodilla.exception.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.stream;
+
+public class Warehouse {
+    public  List<Order> orders = new ArrayList<>();
+
+    public  Order addOrder(Order order) {
+        orders.add(new Order(""));
+        return new Order("");
+    }
+
+    public Order getOrder(String number) {
+        Order order = new Order(number);
+        return orders.stream()
+                .filter(o -> o.getNumber().equals(number))
+                .findAny()
+                .orElse(null);
+    }
+
+
+    public boolean isOrderInUse (String order) throws OrderDoesntExistException {
+        if (orders.contains(order))
+            return orders.contains(order);
+        throw  new OrderDoesntExistException();
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -0,0 +1,17 @@
+package com.kodilla.exception.homework;
+
+public class WarehouseApp {
+    public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("Order1"));
+        warehouse.addOrder(new Order("Order2"));
+        warehouse.addOrder(new Order("Order3"));
+
+        try {
+            boolean isOrderCorrect = warehouse.isOrderInUse("Order3");
+            System.out.println("Order correct");
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Order not correct");
+        }
+    }
+}
