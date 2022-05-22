426335bdc7a76d1c8f4e868333ae7a7fb36606e3
Michal Granatowski <m.granatowski@gmail.com>
2022-05-22 21:11:47 +0200

    ShippingCenter2

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -3,9 +3,9 @@ package com.kodilla.spring.basic.dependency_injection.homework;
 public class Application {
 
     public static void main(String[] args) {
-        NotificationInterface notificationInterface = new NotificationService();
-        DeliveryInterface deliveryInterface = new DeliveryService();
-        ShippingCenter application = new ShippingCenter(notificationInterface, deliveryInterface);
+        NotificationService notificationService = new NotificationService();
+        DeliveryService deliveryService = new DeliveryService();
+        ShippingCenter application = new ShippingCenter(deliveryService, notificationService);
         application.sendPackage("Hill Street 11, New York", 18.2);
     }
 }
