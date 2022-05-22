79b977696fdab64720763a92d6456b9ed36dbf65
Michal Granatowski <m.granatowski@gmail.com>
2022-05-22 22:09:03 +0200

    UserValidator

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.parametrized_tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringManipulatorTestSuite {
+    private StringManipulator manipulator = new StringManipulator();
+
+    @ParameterizedTest
+    @CsvSource(value = {"test,tset", "OtHEr,rehto", "EVent,tneve", "null,llun", "A,a"})
+    public void shouldReverseStringWithLowerCase(String input, String expected) {
+        assertEquals(expected, manipulator.reverseWithLowerCase(input));
+    }
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/UserValidatorTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.parametrized_tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserValidatorTestSuite {
+
+    private UserValidator validator = new UserValidator();
+
+    @Test
+    public void isUsernameOk(){
+        assertTrue(validator.validateUsername("Michal8"));
+    }
+    @Test
+    public void isUsernameOk2(){
+        assertTrue(validator.validateUsername("Mich  al8"));
+    }
+    @Test
+    public void isEmailOk(){
+        assertTrue(validator.validateEmail("Michal8@onet.pl"));
+    }
+    @Test
+    public void isEmailOk2(){
+        assertTrue(validator.validateEmail("Michal@8@onet.pl"));
+    }
+}
