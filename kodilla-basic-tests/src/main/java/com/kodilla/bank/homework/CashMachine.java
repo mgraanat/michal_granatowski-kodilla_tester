24779021cfa008b40e94e99f602584526fac571a
Michal Granatowski <m.granatowski@gmail.com>
2022-06-14 00:33:31 +0200

    Cashmachine2

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -4,21 +4,44 @@ public class CashMachine {
 
     private int[] transactions;
     private int size;
-    private int cashMachineBalance = 0;
+    private int balance;
 
     public CashMachine() {
         this.transactions = new int[0];
     }
 
-    public void add (int transaction) {
-        this.size++;
-        int[] newTab = new int[this.size];
-        System.arraycopy(transactions, 0, newTab,0, transactions.length);
-        newTab[this.size - 1] = transaction;
-        this.transactions = newTab;
-    }
     public int[] getTransactions() {
         return transactions;
     }
-    public int depositMoney;
+
+    public int checkBalance() {
+        int balance = 0;
+        for (int i = 0; i < transactions.length; i++) {
+            balance += transactions[i];
+        }
+        return balance;
+    }
+
+    public int getNumberOfTransactions() {
+        return transactions.length;
+    }
+
+
+    public void add(int transaction) {
+        this.size++;
+        int[] newTab = new int[this.size];
+        System.arraycopy(transactions, 0, newTab, 0, transactions.length);
+        newTab[this.size - 1] = transaction;
+        this.transactions = newTab;
+    }
+    public int payment (int value){
+        balance = balance + value;
+        add(value);
+        return balance;
+    }
+    public int cashWithdrawal(int value){
+        balance = balance - value;
+        add(-value);
+        return balance;
+    }
 }
