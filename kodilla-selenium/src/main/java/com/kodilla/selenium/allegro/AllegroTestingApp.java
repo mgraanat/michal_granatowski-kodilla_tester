984436ffc03acf664916879829f09c67b12b45c9
Michal Granatowski <m.granatowski@gmail.com>
2022-06-19 23:12:01 +0200

    13.5

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -16,12 +19,19 @@ public class AllegroTestingApp {
         //Alert alert = driver.switchTo().alert();
         //alert.accept();
 
-        WebElement categoryCombo = driver.findElement(By.xpath("//*[@data-role=\"header-wrapper\"]/div/span/div/select"));
+        WebElement categoryCombo = driver.findElement(By.className(".mpof_5r mpof_ki_s mp7g_oh m389_6m mjyo_6x mse2_40 mp4t_0 mr3m_0 mli2_0 mx7m_1 m911_co mnyp_co mdwl_co mefy_5r msts_pt mlkp_6x _535b5_PqNDg")));
         Select category = new Select(categoryCombo);
         category.selectByIndex(3);
 
-        WebElement inputField = driver.findElement(By.xpath("//*[@name=\"string\"]"));
+        WebElement inputField = driver.findElement(By.className(".msts_pt mgmw_wo mr3m_1 mli2_1 mjyo_6x mse2_40 mqu1_40 mp4t_0 m3h2_0 mryx_0 munh_0 mg9e_0 mj7a_0 mh36_0 mvrt_8 mlkp_6x mefy_5r mldj_0 mm2b_0 ozg4u mh85_0 _535b5_nwl41")));
         inputField.sendKeys("mavic mini");
         inputField.submit();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
+        List<WebElement> elements = driver.findElements(By.cssSelector("#article"));
+        elements.get(0).getText();
+
+
     }
 }
